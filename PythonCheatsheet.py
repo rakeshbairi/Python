81cedeafe7abac036a4a6fba02d673b11a017f7c
rakeshbairi <bairi.rakesh@gmail.com>
2019-05-21 17:53:13 +0100

    Add on features

--- PythonCheatsheet.py
+++ PythonCheatsheet.py
@@ -6,6 +6,8 @@ last_name = 'einstein'
 full_name = first_name + ' ' + last_name
 print(full_name)
 
+#A list stores a series of items in a particular order. You
+#access items using an index, or within a loop.
 
 #Make a list
 bikes = ['trek', 'redline', 'giant']
@@ -30,16 +32,18 @@ bikes.append('trek')
 bikes.append('redline')
 bikes.append('giant')
 
-#Making numerical lists
-#range(start, stop, step)
-#start	Optional. An integer number specifying at which position to start. Default is 0
-#stop	Optional. An integer number specifying at which position to end.
-#step	Optional. An integer number specifying the incrementation. Default is 1
+'''
+Making numerical lists
+range(start, stop, step)
+start	Optional. An integer number specifying at which position to start. Default is 0
+stop	Optional. An integer number specifying at which position to end.
+step	Optional. An integer number specifying the incrementation. Default is 1
+'''
 
 squares = []
 for x in range(1, 11):
-    print(x)
-    squares.append(x**2) 
+ #   print(x)
+   squares.append(x**2) 
 
 for item in squares:
   print(item)
@@ -47,3 +51,40 @@ for item in squares:
 squares = [x**2 for x in range(1, 11)]
 
 #Slicing a list
+finishers = ['sam', 'bob', 'ada', 'bea']
+first_two = finishers[:2]
+
+for item in first_two:
+  print(item)
+
+#Copying a list
+  copy_of_bikes = bikes[:]
+
+#Tuples
+#Tuples are similar to lists, but the items in a tuple can't be modified.
+dimensions = (1920, 1080)
+
+#Conditional test with lists
+#equals x == 42 not equal x != 42 greater than x > 42 or equal to x >= 42 less than x < 42 or equal to x <= 42
+
+#'trek' in bikes 
+#'surly' not in bikes
+age=20
+if age >= 18: 
+  print("You can vote!")
+
+age=2
+if age < 4: 
+   ticket_price = 0 
+elif age < 18:
+   ticket_price = 10 
+else: 
+   ticket_price = 15
+    
+print(ticket_price)
+
+#Dictionaries store connections between pieces of information. Each item in a dictionary is a key-value pair.
+alien = {'color': 'green', 'points': 5}
+print("The alien's color is " + alien['color'])
+alien['x_position'] = '0'
+print("The alien's color is " + alien['x_position'])
